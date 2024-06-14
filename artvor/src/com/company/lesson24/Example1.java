7fab40650e249778842991396c67d18d6d28a375
telpvn <telpvn@mail.ru>
2024-06-14 20:45:35 +0400

    First introduction to exceptions 1

---
+++ artvor/src/com/company/lesson24/Example1.java
@@ -0,0 +1,19 @@
+package com.company.lesson24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Example1 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String s = null;
+        System.out.println(s.toString());
+    }
+}
